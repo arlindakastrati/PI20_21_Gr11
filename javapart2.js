da93770e18a829803fdf9e204dd1bdbc8a293ad0
arlindakastrati <74983643+arlindakastrati@users.noreply.github.com>
2021-03-26 14:21:55 +0100

    Add files via upload

---
+++ javapart2.js
@@ -0,0 +1,9 @@
+
+
+function myram(){
+    document.getElementById("Ram").innerHTML="Random-access memory";}
+function locvar() {
+ var a = "to continue?";
+alert("Are you still OK " + a); 
+}
+locvar();
