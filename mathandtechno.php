62d82d5d30c1f671c1258ec9e4cc63265702ce73
arlindakastrati <74983643+arlindakastrati@users.noreply.github.com>
2021-04-23 14:49:46 +0200

    Converting
    Converting a html file into a php in order to be executed

---
+++ mathandtechno.php
@@ -0,0 +1,399 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>STEM-Science Technology Engineering Mathematics</title>
+  <style>
+    *{
+      background-color:rgb(126, 164, 179);
+    }
+    body{
+      margin:50px 50px
+    }
+    p{
+      font-size: 22px;
+      color: white;
+      font-family: Arial, Helvetica, sans-serif;
+    }
+    h1{
+      text-align: center;
+      font-size: 50px;
+      color: rgb(7, 72, 102);
+      text-shadow:1px 6px 10px aliceblue ;
+    }
+    h2{
+      font-size: 40px;
+      color:rgb(6, 82, 117);
+    }
+    form {
+    height:435px;
+    width:350px;
+    text-align:center;
+    border:5px solid rgb(53, 50, 50);
+    border-radius:20%;
+    margin-left: 1060px;
+    background-color: rgb(6, 82, 117);
+    margin-top: -940px;
+   }
+  </style>
+  
+
+</head>
+<body>
+  <h1> Science Technology Engineering Mathematics</h1>
+    <h2>The Importance of STEM</h2>
+    <p style="width: 730px;">Technology provides dynamic opportunities for instruction in math and STEM classrooms.
+       We can enhance the learning process and make concepts come alive through engaging and 
+       interactive media. We may also offer additional supports to address the needs of all
+       learners and create customized learning experiences. Here are some important ways that
+       students can benefit when we incorporate technology with our math and STEM lesson instruction.</p>
+
+    <p style="width: 730px;">Multimedia brings learning to life! We can bring videos, animations, interesting movies and 
+       other media into the learning process to help our students develop skills and understandings. 
+       And it can help to motivate and excite our students about their learning!</p>
+
+    <img src="mathtech2.jpg" alt="Stem" width="650px" height="350px" style="margin-left:800px;margin-top: -330px;">
+
+       <h2>Interactive Explorations and Visualizations</h2>
+       <img src="mathtech3.jpg" alt="Stem" width="650px" height="400px" >
+
+       <p style="width: 730;margin-left: 740px;margin-top: -400px;">Making math (or STEM subjects) visual goes beyond student engagement;
+      <strong><a href="https://www.youcubed.org/resource/visual-mathematics/" target="_blank">
+       brain research indicates it to be integral to learning maths</a>.</strong> Neuroscientists 
+       at Stanford University are studying how the brain thinks mathematically and evidence shows 
+       that visual pathways are involved even when working on symbolic number calculations. 
+       According to Jo Boaler and the team at Stanford Graduate School of Education&rsquo;s 
+       <a href="https://www.youcubed.org/" target="_blank"><strong>youcubed</strong></a>,
+        representing all mathematical concepts visually, and including visual activities at 
+        all grade levels, can greatly help students.<br />
+       <br />
+       Technology provides additional opportunities for learners to see and interact with 
+       mathematical concepts. Students can explore and make discoveries with games, simulations and digital tools.<br />
+       <br />
+       One excellent platform for teachers and students is the web-based graphing
+        calculator, <a href="https://www.desmos.com/" target="_blank"><strong>Desmos</strong></a>.
+        The <strong><a href="https://teacher.desmos.com/" target="_blank">Desmos classroom 
+            activities page</a></strong> is a great starting point to engage students in
+         playing with and testing mathematical ideas and also sharing and collaborating.<br />
+       <br /></p>
+
+      <p>As we know educators prepare students for a world where change happens at a rapid pace,
+       they use multiple tools to communicate lessons. More often than not, new classroom technology
+       is implemented in conjunction with, rather than instead of, more traditional classroom tools,
+       according to a new report from Cambridge International, which is based on an online survey of
+       nearly 20,000 teachers and students (ages 12–19) from 100 countries.</p>
+       <p ><?php   
+    
+       /* Vargjet asociative  */
+       $techpercent["Desktop computer"] = 38; 
+       $techpercent["Smartphone"] = 26; 
+       $techpercent["Interactive whiteboards"] = 10; 
+       $techpercent["Notebook"] = 11; 
+       $techpercent["Tablets"] = 15; 
+    
+      /* Aksesimi i elementeve ne menyre direkte */
+      echo "The survey found that use of technology in schools worldwide continues to grow:\n";
+       ?> </p>
+      <p style="color: rgb(7, 72, 102);"><?php
+      echo "- Desktop computer:" . $techpercent["Desktop computer"], "%"; 
+      echo "<br>";
+      echo "- Smartphone:" . $techpercent["Smartphone"], "%"; 
+      echo "<br>";
+      echo "- Interactive whiteboards:" . $techpercent["Interactive whiteboards"], "%"; 
+      echo "<br>";
+      echo "- Notebook:" . $techpercent["Notebook"], "%"; 
+      echo "<br>";
+      echo "- Tablets:" . $techpercent["Tablets"], "%"; 
+      echo "<br>";
+      ?> </p>
+      <p> And, the new addition to Texthelp&rsquo;s STEM offering,
+        <a href="https://equatio.texthelp.com/" target="_blank"><strong>
+        EquatIO mathspace</strong></a>, creates a digital whiteboard where
+        students and teachers can combine math equations and formulas with 
+        Desmos graphs, geometrics shapes, manipulatives, and freehand drawings to
+         encourage visual problem solving.<br /> </p>
+       <p>Click the button to test if the word <mark style="background-color:rgb(41, 144, 192) ;">"addition "</mark> from the string that you  did  read before  exist.  <button onclick="aboveand()" style="border: solid 2px;
+         color:rgb(6, 82, 117);background-color:rgb(41, 144, 192) ;width: 1.5cm;height:1 cm;border-radius: 30%;">Test it</button></p>
+        <p id="first space" style="color: black;"></p>
+       <p style="margin-top:-15px;">Click the button to print the word <mark style="background-color:rgb(41, 144, 192) ">"addition"</mark> from the string that you read before if it is true.  <button onclick="aboveandbelow()" style="border: solid 2px;
+        color:rgb(6, 82, 117);background-color:rgb(41, 144, 192) ;width: 1.5cm;height:1 cm;border-radius: 30%;">See it</button></p>
+       <p id="new space" style="color: black;"></p>
+      
+   
+
+
+       <h2>Personalized STEM lessons using technology</h2>
+       <p style="width: 730px;">   Increased access to technology for STEM allows for a more customized learning experience.
+             Because no two learners are exactly alike, technology can provide individual students
+             with content and supports that are particularly helpful to their individual needs.
+             Kids can view lessons, tutorials, screencasts, and other instructional media on their
+             own device and at their own pace. So if one student is still confused on a topic, 
+             and another is ready for additional challenges, technology can enable each to take 
+             the appropriate next step.</p>
+       
+        <img src="mathtech1.jpg" alt="Stem" width="650px" height="350px"style="margin-left:800px; margin-top:-200px;">
+
+        <p style="width: 730px; margin-top: -150px;">A great example of how technology empowers learners is the phenomenon of
+          <strong><a href="https://www.khanacademy.org/" target="_blank">Khan Academy</a></strong>
+         Sal Khan did not intend to build a non-profit educational organization when 
+        he started posting the recordings of his math lessons on YouTube (as he 
+        delightfully explains in his TED Talk). He was only trying to help tutor
+        his cousins from afar and didn’t see any reason to set the videos to 
+        Private mode. From the feedback from his cousins, and then from other
+        people from all over the world who found his videos, he realized how 
+        valuable this medium was and the importance of being able to choose,
+        rewind, and control the lessons.</p>
+        
+
+        <p>The Khan Academy platform has emerged from his work, giving learners personalized 
+        learning experiences in a number of ways. For example, users can take quizzes to see 
+        what concepts they have mastered and what they need more practice with. Or students 
+        that learn better through written text can access transcripts that accompany the 
+        instructional videos. Providing the ability to direct and control learning pathways
+         is a powerful reason to include technology in our own math instruction.</p>
+        <p><?php
+        function quotes() {
+        echo "Integrating technology in the math classroom allows students to interact with 
+        people outside of the classroom to help broaden their understandings and perspectives
+        about what they are studying.";
+         }
+        quotes(); // call the function
+        ?></p>
+
+        <h2> A simple example of the impact of the technology in calculators</h2>
+        <p style="width: 850px;">The calculator has had a profound impact on the world, making computations quicker and 
+          more exact. In the classroom, calculators have given many students the ability to learn 
+          about and put complex formulas and concepts into practice more easily. Especially in
+          lower-grade mathematics courses, some instructors still don't allow their use to make
+          sure students truly understand mathematical concepts and learn problem-solving techniques. 
+          But for many calculus and trigonometry courses in high school, for example, graphing 
+          calculators are a requirement.<br>
+          <img src="mathteach4.jpg" alt="calc" width="400px" height="265px"style="margin-top:20px;" >
+          <img src="mathtech5.jpg" alt="calcapp" width="200"style="margin-left:50px;" ></p>
+         
+          
+<?php
+$positivees = array('Invention', 'Innovation', 'Imitation','Learning');
+$negativees = array('Isolation', 'Depression', 'Eyestrain','Anexity');
+
+?>
+<h2>Positive Impact of Technology  <span style="color:white;"> vs</span>    Negative Impact of Technology</h2>
+<ul style="color:white;font-size:22px;font-family: Arial, Helvetica, sans-serif;
+    }">
+<?php foreach ($positivees as $positive) { ?>
+<li><?php echo $positive ?></li>
+<?php }  ?>
+</ul>
+
+<ul style="color:white;font-size:22px;font-family: Arial, Helvetica, sans-serif;margin-left:600px;margin-top:-130px;}">
+<?php foreach ($negativees as $negative) { ?>
+<li><?php echo $negative ?></li>
+<?php }  ?>
+</ul>
+<p>
+<?php
+$quote = "In your right there is a simple calculator that we created.";
+echo "Calculators have even moved online and have a number of practical applications.
+Engineers continue to make advancements in calculator technology, and as they become
+more and more complex, the lines between personal computers and classic calculators may continue to blur. $quote!";
+?></p>
+<p ><?php
+define("Thanks", "Hoping that you gained some important information from our page!");
+echo Thanks;
+?>
+</p>
+<p style="margin-left:1200px;color:rgb(7, 72, 102)"><?php
+$day = date("D");
+if($day == "Fri"){
+    echo "Have a nice weekend!";
+}
+?></p>
+
+<p id="data" style=" border: none;color: rgb(7, 72, 102); background-color:rgb(126, 164, 179);"></p>
+
+<script>
+var d = new Date();
+document.getElementById("data").innerHTML = d;
+</script>
+
+
+        
+          
+
+  <form name="calculator">
+    <h2 style="background-color: transparent; color: white;">Calculator </h2>
+    <input type="text" name="text1" placeholder="First Number" style="height:30px; width: 200px;border:solid rgb(53, 50, 50)"><br><br>
+    <input type="text" name="text2" placeholder="Second Number" style="height:30px; width: 200px;border:solid rgb(53, 50, 50)"><br><br>
+    <input type="button" value="+" onClick="javascript:add();" style="border: solid rgb(53, 50, 50);width:1cm;height: 0.8cm;font-size: 22px;">
+    <input type="button" value="-" onClick="javascript:subtract();"style="border: solid rgb(53, 50, 50);width:1cm;height: 0.8cm;font-size: 22px;">
+    <input type="button" value="×" onClick="javascript:multiply();"style="border: solid rgb(53, 50, 50);width:1cm;height: 0.8cm;font-size: 22px;">
+    <input type="button" value=":" onClick="javascript:divide();"style="border: solid rgb(53, 50, 50);width:1cm;height: 0.8cm;font-size: 22px;"><br><br>
+    <input type="button" value="%" onClick="javascript:modulus();"style="border: solid rgb(53, 50, 50);width:1cm;height: 0.8cm;font-size: 14px;">
+    <input type="button" value="Exp" onClick="javascript:exponential();"style="border: solid rgb(53, 50, 50);width:1cm;height: 0.8cm;font-size: 14px;">
+    <input type="button" value="Str" onClick="javascript:string();"style="border: solid rgb(53, 50, 50);width:1cm;height: 0.8cm;font-size: 14px;">
+    <input type="button" value="Fix" onClick="javascript:fix();"style="border: solid rgb(53, 50, 50);width:1cm;height: 0.8cm;font-size: 14px;"><br><br>
+    <input type="button" value="π" onClick="javascript:pifunction();"style="border: solid rgb(53, 50, 50);width:1cm;height: 0.8cm;font-size: 14px; left:0.5cm;">
+    <input type="button" value="e" onClick="javascript:efunction();"style="border: solid rgb(53, 50, 50);width:1cm;height: 0.8cm;font-size: 14px;">
+    <input type="button" value="ln2" onClick="javascript:ln2function();"style="border: solid rgb(53, 50, 50);width:1cm;height: 0.8cm;font-size: 14px;">
+    <input type="button" value="ln10" onClick="javascript:ln10function()();"style="border: solid rgb(53, 50, 50);width:1cm;height: 0.8cm;font-size: 14px;"><br><br>
+
+
+
+<b style="background-color: transparent; color:white;font-size: 22px;">Result:</b><br>
+<input type="text" name="total" disabled style="
+border:3px solid rgb(53, 50, 50);height:0.7cm;">
+</form>
+<script language="javascript" type="text/javascript">
+const PI = 3.141592653589793;
+const E = 2.7182818284;
+const LN2 = 0.6931471805599453;
+const LN10= 2.302585092994046;
+  // Addition Operator 
+  function add(){
+  a=Number(document.calculator.text1.value);
+  b=Number(document.calculator.text2.value);
+  c=a+b;
+  document.calculator.total.value=c;
+  }
+  //Subtraction Operator
+  function subtract(){
+  a=Number(document.calculator.text1.value);
+  b=Number(document.calculator.text2.value);
+  c=a-b;
+  document.calculator.total.value=c;
+  }
+  //Multiplication Operator
+  function multiply(){
+  a=Number(document.calculator.text1.value);
+  b=Number(document.calculator.text2.value);
+  c=a*b;
+  document.calculator.total.value=c;
+  }
+  //Division Operator
+  function divide(){
+  a=Number(document.calculator.text1.value);
+  b=Number(document.calculator.text2.value);
+  c=a/b;
+  document.calculator.total.value=c;
+  }
+  //Modulo Operator
+  function modulus(){
+  a=Number(document.calculator.text1.value);
+  b=Number(document.calculator.text2.value);
+  c=a%b;
+  document.calculator.total.value=c;
+  }
+  //Exponential function
+  function exponential(){
+      a=Number(document.calculator.text1.value);
+      b=Number(document.calculator.text2.value);
+      c=a.toExponential();
+      c=b.toExponential();
+      document.calculator.total.value=c;
+      
+       
+  }
+  //String function
+   function string(){
+          a=Number(document.calculator.text1.value);
+          b=Number(document.calculator.text2.value);
+          c=a.toString();
+          c=b.toString();
+          document.calculator.total.value=c; 
+      }
+  //Fixed function
+      function fix(){
+          a=Number(document.calculator.text1.value);
+          b=Number(document.calculator.text2.value);
+          c=a.toFixed();
+          c=a.toFixed();
+          document.calculator.total.value=c;
+      }
+
+    //pifunction
+    function pifunction(){
+          a=Number(document.calculator.text1.value);
+          b=Number(document.calculator.text2.value);
+          c=a*PI;
+          c=b*PI;
+          c=(a+b)*PI;
+          document.calculator.total.value=c;  
+      }
+    // E function
+    function efunction(){
+          a=Number(document.calculator.text1.value);
+          b=Number(document.calculator.text2.value);
+          c=a*E;
+          c=b*E;
+          c=(a+b)*E;
+          document.calculator.total.value=c;  
+      }
+    // Ln2 function
+    function ln2function(){
+          a=Number(document.calculator.text1.value);
+          b=Number(document.calculator.text2.value);
+          c=a*LN2;
+          c=b*LN2;
+          c=(a+b)*LN2;
+          document.calculator.total.value=c;  
+      }
+    // Ln10 function
+    function ln10function(){
+          a=Number(document.calculator.text1.value);
+          b=Number(document.calculator.text2.value);
+          c=a*LN10;
+          c=b*LN10;
+          c=(a+b)*LN10;
+          document.calculator.total.value=c;  
+      }
+      function aboveand() {
+      var abovee = "And, the new addition to Texthelp’s STEM offering, EquatIO mathspace, creates a digital whiteboard where students and teachers can combine math equations and formulas with Desmos graphs, geometrics shapes, manipulatives, and freehand drawings to encourage visual problem solving.";
+      var beloww = new RegExp("addition");
+      var inlinee = beloww.test(abovee);
+      document.getElementById("first space").innerHTML = inlinee;
+      }
+      function aboveandbelow() {
+      var above = "And, the new addition to Texthelp’s STEM offering, EquatIO mathspace, creates a digital whiteboard where students and teachers can combine math equations and formulas with Desmos graphs, geometrics shapes, manipulatives, and freehand drawings to encourage visual problem solving.";
+      var below = new RegExp("addition");
+      var inline = below.exec(above);
+      document.getElementById("new space").innerHTML = inline;
+     }
+     
+
+     function Worldii(word,name){
+       this.word = word;
+       this.name = name;
+     }
+     function Worldiii(word, name,year) {
+     this.word = word;
+     this.name = name;
+     this.year = year;
+     
+     }
+     function theimpact(quotes){
+     console.log('\'' + quotes.word  + quotes.name +         ' that founded Microsoft Windows in the year ' + quotes.year + '.\'');
+     }
+     function theimpacti(quotes){
+     console.log('\'' + quotes.word  + quotes.name +   ' is probably best known as the famous CEO of Tesla Motors and SpaceX (especially last year).\'' );
+     }
+     function theimpactii(quotes){
+     console.log('\'' + quotes.word  + quotes.name +   '  to thank for the trusty search engine founded in  ' + quotes.year + '.\'' );
+     }
+     var headding ="Three people who left an impact in the technology world?";
+     var person1 = new Worldiii('One of the biggest names in computer science and culture as we know it,', 'Bill Gates', 1975 );
+     var person2 = new Worldii('Referred to as the real Iron Man ', 'Elon Musk');
+     var person3 = new Worldiii('Can you go a week without Googling something? There is ', 'Larry Page', 1998);
+     console.log(headding);
+     theimpact(person1);
+     theimpacti(person2);
+     theimpactii(person3);
+    
+      
+      
+  </script>
+
+
+
+
+</body>
+</html>
